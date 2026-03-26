bb8e4214e0c7dc8b179f7c523f571e8352c2adb4
kaviya-sharon14 <sharonkaviya70@gmail.com>
2026-03-26 15:22:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",   // ✅ ADD THIS LINE
+  base: "./",  // ✅ IMPORTANT FIX
 
   server: {
     host: "::",
