4874547b70b40d3afb95ffec190e2e8648109ecd
kaviya-sharon14 <sharonkaviya70@gmail.com>
2026-03-26 16:14:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-```javascript
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -20,4 +20,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-```
